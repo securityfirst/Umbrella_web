3cfd622739e4524b446d3ae8f5188e7bdfbcbe49
Sun Lee <hi@sunlee.io>
2018-12-20 19:51:03 -0500

    Finish FeedsAll UI, add button to FeedsRss, add feeds mock

--- components/feeds/FeedsAll.js
+++ components/feeds/FeedsAll.js
@@ -8,11 +8,16 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
-import TouchRipple from '@material-ui/core/ButtonBase/TouchRipple';
+import Paper from '@material-ui/core/Paper';
 
 import red from '@material-ui/core/colors/red';
 
+import { feeds } from '../../mock/feeds';
+
 const styles = theme => ({
+	wrapper: {
+		position: 'relative',
+	},
 	heading: {
 		flexBasis: '33.33%',
 		flexShrink: 0,
@@ -35,6 +40,29 @@ const styles = theme => ({
 		textAlign: 'right',
 		cursor: 'pointer',
 	},
+	feed: {
+		...theme.mixins.gutters(),
+		margin: '.75rem 0',
+		paddingTop: theme.spacing.unit * 2,
+		paddingBottom: theme.spacing.unit * 2,
+	},
+	feedTitle: {
+		fontSize: '1.125rem',
+	},
+	feedSite: {
+		display: 'inline-block',
+		paddingRight: '.75rem',
+		color: theme.palette.grey[600],
+		fontSize: '.75rem',
+	},
+	feedDate: {
+		display: 'inline-block',
+		fontSize: '.75rem',
+	},
+	feedContent: {
+		color: theme.palette.grey[800],
+		fontSize: '.875rem',
+	},
 });
 
 class FeedsAll extends React.Component {
@@ -50,8 +78,10 @@ class FeedsAll extends React.Component {
 		const { classes, toggleEdit } = this.props;
 		const { expanded } = this.state;
 
+		console.log("feeds: ", feeds);
+
 		return (
-			<div>
+			<div className={classes.wrapper}>
 				<ExpansionPanel expanded={expanded === true} onChange={this.handleChange}>
 					<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
 						<Typography className={classes.heading}>Location: 
@@ -63,6 +93,19 @@ class FeedsAll extends React.Component {
 						<Typography variant="body1" className={classes.locationChangeLink} onClick={toggleEdit}>Change</Typography>
 					</ExpansionPanelDetails>
 				</ExpansionPanel>
+
+				{feeds.map((feed, i) => {
+					return (
+						<Paper key={i} className={classes.feed}>
+							<Typography className={classes.feedTitle} variant="h6">{feed.title}</Typography>
+							<Typography paragraph>
+								<Typography className={classes.feedSite}>Via {(feed.site || "").toUpperCase()}</Typography>
+								<Typography className={classes.feedDate}>{new Date(feed.timestamp).toLocaleString()}</Typography>
+							</Typography>
+							<Typography className={classes.feedContent} paragraph>{feed.content}</Typography>
+						</Paper>
+					);
+				})}
 			</div>
 		);
 	}
--- components/feeds/FeedsRss.js
+++ components/feeds/FeedsRss.js
@@ -3,26 +3,45 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 import { contentStyles } from '../../utils/view';
 
-const styles = theme => contentStyles(theme);
+const styles = theme => ({
+	addButton: {
+		position: 'fixed',
+		top: '25vh',
+		right: '5vw',
+	},
+	addIcon: {
+		color: theme.palette.common.white,
+	},
+});
 
 class FeedsRss extends React.Component {
 	render() {
+		const { classes } = this.props;
+
 		return (
-			<Typography paragraph>
-				<strong>FEEDS RSS</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-				incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-				elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-				hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-				velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-				Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-				viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-				Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-				at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-				ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-			</Typography>
+			<div>
+				<Typography paragraph>
+					<strong>FEEDS RSS</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+					elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+					hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+					velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+					Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+					viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
+					Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
+					at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+					ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+				</Typography>
+				
+				<Fab className={classes.addButton} color="secondary" aria-label="Add">
+					<AddIcon className={classes.addIcon} />
+				</Fab>
+			</div>
 		);
 	}
 }
