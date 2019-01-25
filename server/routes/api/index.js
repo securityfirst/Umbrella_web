cdb84d0936d9115a4b05388f4a5cfb30d5a9c2c7
Sun Lee <hi@sunlee.io>
2019-01-24 23:15:16 -0500

    Add CORS to API routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const cors = require("cors");
 
-const github = require('./github')
+const github = require('./github');
 
+const whitelist = [process.env.ROOT];
+
+const corsOptions = {
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) callback(null, true);
+    else callback(new Error('Not allowed by CORS'));
+  }
+};
+
+router.use(cors(corsOptions));
 router.use('/github', github);
 
 module.exports = router;
