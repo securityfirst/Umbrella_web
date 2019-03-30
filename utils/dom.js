7a56552ce0e2894aadee737d2eaebe9801d340bc
Sun Lee <hi@sunlee.io>
2019-03-30 08:08:24 -0400

    Finish copy share link

--- utils/dom.js
+++ utils/dom.js
@@ -115,24 +115,3 @@ export const download = (name, sha) => {
 		})
 
 }
-
-export const copy = val => {
-	if (!val) {
-		alert('Something went wrong. Please refresh the page and try again.')
-		return false
-	}
-
-	if (!document.queryCommandSupported('copy')) {
-		alert('This is not supported by your browser.')
-		return false
-	}
-
-	const el = document.createElement('textarea')
-	el.value = val
-	document.body.appendChild(el)
-	el.select()
-	const success = document.execCommand('copy')
-	document.body.removeChild(el)
-
-	return success
-}
