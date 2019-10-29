445a08f8310ecab7b5691da4afec7c808c77fccd
Sun Lee <hi@sunlee.io>
2019-10-29 13:15:48 -0400

    Add share to new forms

--- pages/forms/index.js
+++ pages/forms/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import marked from 'marked'
 import { connect } from 'react-redux'
 
 import { withStyles } from '@material-ui/core/styles'
@@ -7,8 +8,18 @@ import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 import Menu from '@material-ui/core/Menu'
+import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
+import Tooltip from '@material-ui/core/Tooltip'
+import Collapse from '@material-ui/core/Collapse'
+
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import ShareIcon from '@material-ui/icons/Share'
+import GetAppIcon from '@material-ui/icons/GetApp'
+import LinkIcon from '@material-ui/icons/Link'
 
 import Layout from '../../components/layout'
 import Loading from '../../components/common/Loading'
@@ -20,6 +31,7 @@ import { setAppbarTitle, openAlert } from '../../store/actions/view'
 import { contentStyles, paperStyles, buttonWrapperStyles } from '../../utils/view'
 import { generateForm } from '../../utils/forms'
 import { downloadPdf, downloadHtml, downloadDocx } from '../../utils/dom'
+import { decodeBlob } from '../../utils/github'
 
 const styles = theme => ({
 	...contentStyles(theme),
@@ -47,7 +59,10 @@ const styles = theme => ({
 
 class Forms extends React.Component {
 	state = {
-		anchorEl: null,
+		activeAnchorEl: null,
+		newAnchorEl: null,
+		downloadOpen: false,
+		tooltipOpen: false,
 	}
 
 	componentDidMount() {
@@ -70,10 +85,6 @@ class Forms extends React.Component {
 			return dispatch(openAlert('error', systemLocaleMap[locale].login_your_password))
 		}
 	}
-
-	handleClick = e => this.setState({ anchorEl: e.currentTarget })
-
-	handleClose = () => this.setState({ anchorEl: null })
 
 	downloadHtml = formSaved => async () => {
 		const { dispatch, locale, systemLocaleMap, form } = this.props
@@ -130,6 +141,95 @@ class Forms extends React.Component {
 			dispatch(openAlert('error', systemLocaleMap[locale].general_error))
 		}
 	}
+	
+	handleCopyLink = () => {
+		const { dispatch, locale, systemLocaleMap } = this.props
+
+		if (typeof document === 'undefined') return false
+
+		if (!document.queryCommandSupported('copy')) {
+			dispatch(openAlert('error', systemLocaleMap[locale].browser_unsupported))
+			return false
+		}
+
+		this.copyInput.focus()
+		this.copyInput.select()
+
+		if (document.execCommand('copy')) {
+			this.setState({tooltipOpen: true}, () => {
+				setTimeout(() => {
+					this.setState({tooltipOpen: false})
+				}, 1500)
+			})
+		}
+	}
+	
+	toggleDownloadList = () => {
+		this.setState({downloadOpen: !this.state.downloadOpen})
+	}
+
+	downloadNewFormHtml = (form) => () => {
+		const { dispatch, locale, systemLocaleMap } = this.props
+
+		dispatch(openAlert('info', systemLocaleMap[locale].downloading_html))
+
+		fetch(`${process.env.ROOT}/api/github/content/${form.sha}`)
+			.then(res => {
+				if (!res.ok) throw res
+				return res.text()
+			})
+			.then(content => {
+				downloadHtml(form.filename.slice(2, form.filename.length - 4), marked(decodeBlob(content)))
+				dispatch(openAlert('success', systemLocaleMap[locale].downloaded))
+				this.setState({ newAnchorEl: null })
+			})
+			.catch(err => {
+				console.error('downloadNewFormHtml error: ', err)
+				dispatch(openAlert('error', systemLocaleMap[locale].general_error))
+			})
+	}
+
+	downloadNewFormPdf = (form) => () => {
+		const { dispatch, locale, systemLocaleMap } = this.props
+
+		dispatch(openAlert('info',systemLocaleMap[locale].downloading_pdf))
+
+		fetch(`${process.env.ROOT}/api/github/content/${form.sha}`)
+			.then(res => {
+				if (!res.ok) throw res
+				return res.text()
+			})
+			.then(content => {
+				downloadPdf(form.filename.slice(2, form.filename.length - 4), marked(decodeBlob(content)))
+				dispatch(openAlert('success', systemLocaleMap[locale].downloaded))
+				this.setState({ newAnchorEl: null })
+			})
+			.catch(err => {
+				console.error('downloadNewFormPdf error: ', err)
+				dispatch(openAlert('error', systemLocaleMap[locale].general_error))
+			})
+	}
+
+	downloadNewFormDocx = (form) => () => {
+		const { dispatch, locale, systemLocaleMap } = this.props
+
+		dispatch(openAlert('info', systemLocaleMap[locale].downloading_docx))
+
+		fetch(`${process.env.ROOT}/api/github/content/${form.sha}`)
+			.then(res => {
+				if (!res.ok) throw res
+				return res.text()
+			})
+			.then(content => {
+				downloadDocx(form.filename.slice(2, form.filename.length - 4), marked(decodeBlob(content)))
+				dispatch(openAlert('success', systemLocaleMap[locale].downloaded))
+				this.setState({ newAnchorEl: null })
+			})
+			.catch(err => {
+				console.error('downloadNewFormDocx error: ', err)
+				dispatch(openAlert('error', systemLocaleMap[locale].general_error))
+			})
+	}
 
 	handleDelete = formSaved => () => {
 		const { dispatch, locale, systemLocaleMap } = this.props
@@ -143,7 +243,7 @@ class Forms extends React.Component {
 
 	renderActivePanel = (formSaved, i) => {
 		const { classes, locale, systemLocaleMap } = this.props
-		const { anchorEl } = this.state
+		const { activeAnchorEl } = this.state
 
 		return (
 			<Paper key={i} className={classes.formPanel} square>
@@ -164,17 +264,17 @@ class Forms extends React.Component {
 						<Link href={`/forms/${formSaved.sha}/${formSaved.id}`}><Button component="button">{systemLocaleMap[locale].form_menu_edit}</Button></Link>
 						<Button 
 							component="button" 
-							aria-owns={anchorEl ? 'active-download-menu' : undefined}
+							aria-owns={activeAnchorEl ? 'active-download-menu' : undefined}
 							aria-haspopup="true"
-							onClick={this.handleClick}
+							onClick={e => this.setState({ activeAnchorEl: e.currentTarget })}
 						>{systemLocaleMap[locale].download_title}</Button>
 						<Button component="button" color="primary" onClick={this.handleDelete(formSaved)}>{systemLocaleMap[locale].delete}</Button>
 
 						<Menu
 							id={`active-download-menu-${i}`}
-							anchorEl={anchorEl}
-							open={Boolean(anchorEl)}
-							onClose={this.handleClose}
+							anchorEl={activeAnchorEl}
+							open={Boolean(activeAnchorEl)}
+							onClose={() => this.setState({ activeAnchorEl: null })}
 						>
 							<ListItem button onClick={this.downloadHtml(formSaved)}>
 								<ListItemText primary={systemLocaleMap[locale].html_name} />
@@ -194,6 +294,9 @@ class Forms extends React.Component {
 
 	renderAvailablePanel = (form, i) => {
 		const { classes, locale, systemLocaleMap } = this.props
+		const { newAnchorEl, downloadOpen, tooltipOpen } = this.state
+
+		const url = `${process.env.ROOT}/forms/${form.sha}`
 
 		return (
 			<Paper key={i} className={classes.formPanel} square>
@@ -203,9 +306,70 @@ class Forms extends React.Component {
 
 				<div className={classes.formPanelButtonsWrapper}>
 					<div></div>
-					<Link href={`/forms/${form.sha}`}>
-						<Button color="primary" onClick={this.checkLogin}>{systemLocaleMap[locale].message_body_form_new}</Button>
-					</Link>
+					<div>
+						<Button 
+							size="small" 
+							className={classes.cardActionIcon} 
+							aria-owns={newAnchorEl ? 'share-menu' : undefined}
+							aria-haspopup="true"
+							onClick={e => this.setState({ newAnchorEl: e.currentTarget })}
+						>
+							<ShareIcon />
+						</Button>
+						<Link href={`/forms/${form.sha}`}>
+							<Button color="primary" onClick={this.checkLogin}>{systemLocaleMap[locale].message_body_form_new}</Button>
+						</Link>
+						<Menu
+							id="share-menu"
+							anchorEl={newAnchorEl}
+							open={Boolean(newAnchorEl)}
+							onClose={() => this.setState({ newAnchorEl: null })}
+						>
+							<ListItem button onClick={this.toggleDownloadList}>
+								<ListItemIcon>
+									<GetAppIcon />
+								</ListItemIcon>
+								<ListItemText inset primary={systemLocaleMap[locale].download_title} />
+
+								{downloadOpen ? <ExpandLess /> : <ExpandMore />}
+							</ListItem>
+
+							<Collapse in={downloadOpen} timeout="auto" unmountOnExit>
+								<List component="div" disablePadding>
+									<ListItem button onClick={this.downloadNewFormHtml(form)}>
+										<ListItemText primary="HTML" />
+									</ListItem>
+									<ListItem button onClick={this.downloadNewFormPdf(form)}>
+										<ListItemText primary="PDF" />
+									</ListItem>
+									<ListItem button onClick={this.downloadNewFormDocx(form)}>
+										<ListItemText primary="DOCX" />
+									</ListItem>
+								</List>
+							</Collapse>
+
+							{Boolean(url) && <Tooltip
+								open={tooltipOpen}
+								title={systemLocaleMap[locale].copied}
+								placement="right"
+							>
+								<ListItem onClick={this.handleCopyLink}>
+									<ListItemIcon>
+										<LinkIcon />
+									</ListItemIcon>
+									<ListItemText inset primary={systemLocaleMap[locale].copy_link} />
+									<input 
+										ref={el => this.copyInput = el} 
+										className={classes.copyInput} 
+										type="text"
+										tabIndex="-1" 
+										aria-hidden="true" 
+										defaultValue={url}
+									/>
+								</ListItem>
+							</Tooltip>}
+						</Menu>
+					</div>
 				</div>
 			</Paper>
 		)
@@ -227,22 +391,13 @@ class Forms extends React.Component {
 		if (getContentLoading || getFormsSavedLoading) return <Loading />
 		else if (getContentError || getFormsSavedError) return <ErrorMessage error={getContentError || getFormsSavedError} />
 
-		const formsSorted = formsSaved.reduce((acc, form) => {
-			if (form.completed) acc.filled.push(form)
-			else acc.active.push(form)
-			return acc
-		}, {
-			active: [], 
-			filled: [],
-		})
-
 		return (
 			<Layout title={`${systemLocaleMap[locale].app_name} | ${systemLocaleMap[locale].form_title}`} description="Umbrella web application">
 				<div className={classes.content}>
-					{!!formsSorted.active.length &&
+					{!!formsSaved.length &&
 						<React.Fragment>
 							<Typography className={classes.label} variant="subtitle1">{systemLocaleMap[locale].form_title}</Typography>
-							{formsSorted.active.map(this.renderActivePanel)}
+							{formsSaved.map(this.renderActivePanel)}
 						</React.Fragment>
 					}
 
@@ -250,13 +405,6 @@ class Forms extends React.Component {
 					{content[locale].forms.content
 						.filter(form => form.filename.indexOf('f_') === 0)
 						.map(this.renderAvailablePanel)
-					}
-
-					{!!formsSorted.filled.length &&
-						<React.Fragment>
-							<Typography className={classes.label} variant="subtitle1">{systemLocaleMap[locale].message_title_active_forms}</Typography>
-							{formsSorted.filled.map(this.renderActivePanel)}
-						</React.Fragment>
 					}
 				</div>
 			</Layout>
